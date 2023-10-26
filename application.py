2ea056d1e236ae421bdbba5daf14bcbfda894416
hmuri <113418916+hmuri@users.noreply.github.com>
2023-10-26 23:46:09 +0900

    feat: 8-2 실습 완성

--- application.py
+++ application.py
@@ -5,24 +5,43 @@ application = Flask(__name__)
 def hello():
     return render_template("index.html")
 
-@application.route("/index", methods=['POST']) 
-def reg_item_submit_post():
-    # data=request.form
-    # return render_template("result.html", data=data)                      
-# @application.route("/index", methods=['POST'])
-# def reg_item_submit_post():
-    image_file=request.files["file"]
-    image_file.save("static/images/{}".format(image_file.filename))
-    data=request.form
-    print(data)
-    return render_template("result.html", data=data)
+@application.route("/etc")
+def etc():
+    return render_template("etc.html")
+
+@application.route("/ewha")
+def ewha():
+    return render_template("ewha.html")
+
+@application.route("/fashion")
+def fashion():
+    return render_template("fashion.html")
+
+@application.route("/food")
+def food():
+    return render_template("food.html")
+
+@application.route("/home")
+def home():
+    return render_template("home.html")
+
+@application.route("/necessary")
+def necessary():
+    return render_template("necessary.html")
+
+@application.route("/pencil")
+def pencil():
+    return render_template("pencil.html")
+
 
 
 @application.route("/submit_item_post", methods=['POST'])
 def reg_item_submit_post2():
     if request.method == 'POST':
-        data = request.form
-        return render_template("result.html", data=data)
+        image_file=request.files["file"]
+        image_file.save("static/images/{}".format(image_file.filename))
+        data=request.form
+        return render_template("result.html", data=data, img_path="static/images/{}".format(image_file.filename))
 
 if __name__ == "__main__":
  application.run(debug=True)
