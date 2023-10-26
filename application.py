14955fb779bf96ff6dc01ebc64394af39ad214d8
Soyeon-Cha <7103sy@naver.com>
2023-10-26 20:23:00 +0900

    feat: application.py

--- application.py
+++ application.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 application = Flask(__name__)
-
 
 @application.route("/")
 def hello():
- return "Hello goorm!"
+    return render_template("index.html")
 
+@application.route("/index", methods=['POST'])
+def reg_item_submit_post():
+    # data=request.form
+    # return render_template("result.html", data=data)                      
+# @application.route("/index", methods=['POST'])
+# def reg_item_submit_post():
+    image_file=request.files["file"]
+    image_file.save("static/images/{}".format(image_file.filename))
+    data=request.form
+    print(data)
+    return render_template("result.html", data=data)
 
 if __name__ == "__main__":
- application.run(host='0.0.0.0', debug=True)
+ application.run(debug=True)
